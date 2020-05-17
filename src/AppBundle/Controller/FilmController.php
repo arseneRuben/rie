499d647e105a2be9d051a96e79bb8df802510cf9
arseneRuben <fopoar@gmail.com>
2020-05-16 21:05:04 -0400

    commit search video
    Recherche des videos sermon en fonction de l'auteur

--- src/AppBundle/Controller/FilmController.php
+++ src/AppBundle/Controller/FilmController.php
@@ -21,15 +21,19 @@ class FilmController extends Controller
             $author = $_POST["author"];
             if ($author != null) {
                 $em = $this->getDoctrine()->getManager();
-                $journalist = $em->getRepository('ApplicationSonataUserBundle:User')->findOneBy(array('id' => $author));
+                $journalist = $em->getRepository('ApplicationSonataUserBundle:User')->findOneBy(array('username' => $author));
                
                 $journalists = $this->getDoctrine()
                     ->getEntityManager()
                     ->createQuery(
-                        'SELECT u FROM ApplicationSonataUserBundle:User u, AppBundle:Film  f WHERE f.journalist=u.id  ORDER BY f.id ASC'
+                        'SELECT u FROM ApplicationSonataUserBundle:User u, AppBundle:Film  f WHERE f.journalist=u.id  ORDER BY u.username ASC'
                     )->getResult();
-                $films = $em->getRepository('AppBundle:Film')->findBy(array('journalist' => $journalist));
-                return $this->render('film/listfilm.html.twig', array('films' => $films, 'journalists' => $journalists));
+                $films  = $this->getDoctrine()
+                ->getEntityManager()
+                ->createQuery(
+                    'SELECT f FROM ApplicationSonataUserBundle:User u, AppBundle:Film  f WHERE u.id=f.journalist  and u.username=:username ORDER BY f.id ASC'
+                )->setParameter('username', $author)->getResult();
+                return $this->render('film/listfilm.html.twig', array('films' => $films, 'journalists' => $journalists, 'journalist' => $journalist));
             }
         }
         // return new Response("No Students");
